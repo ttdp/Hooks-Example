9ab1e80fbe56c8555990bb74ddc10926de5bfe65
ttdp <tiantong.dev@gmail.com>
2020-12-24 15:13:55 +0800

    Add Greeting

---
+++ src/Greeting.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class Greetings extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { name: "Tong" }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    componentDidMount() {
+        document.title = this.state.name
+    }
+
+    componentDidUpdate() {
+        document.title = this.state.name
+    }
+
+    handleChange(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <label>name: </label>
+                <input value={this.state.name} onChange={this.handleChange}></input>
+            </div>
+        )
+    }
+}
+
+export default Greetings
